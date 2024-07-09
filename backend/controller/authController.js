c1d91428f1283d4597724ccc884a5a59ee34c3fe
Rana Nooris Ur Rehman <rananooris8@gmail.com>
2024-07-10 01:01:40 +0200

    added errorHandler and register auth

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,17 @@
 const authController = {
+    async register(req, res, next) {
+        // validate user input
+        const userRegisterSchema = Joi.object({
+            username: Joi.string().min(5).max(30).required(),
+            name: Joi.string().max(30).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().pattern(passwordPattern).required(),
+            confirmPassword: Joi.ref('password')
+        });
+
+    },
     async login() {},
-    async register() {},
+    
 }
 
 module.exports = authController;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dbConnect = require('./database/index');
 const {PORT} = require('./config/index');
 const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(router);
 
 dbConnect();
 
-// app.get('/', (req, res) => res.json({msg: 'Hello World!'}));
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
